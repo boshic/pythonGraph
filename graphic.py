f849feaf574aea23fa5d40fcf241ab8a1b825503
Boshic <p.vilmen@mail.ru>
2021-09-16 10:09:12 +0300

    naming 16-09-21

--- graphic.py
+++ graphic.py
@@ -3,55 +3,55 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-graphParams = {
-    "minTrshVal" : 0.5,
-    "minTrshColor" : 'r',
-    "clntDetectedColor" : 'g',
-    "clntDetectedVal" : 1,
-    "sizeMultyplier" : 1000,
-    "depthCorrectorKb": 145
+graph_params = {
+    "min_trsh_val" : 0.5,
+    "min_trsh_color" : 'r',
+    "clnt_detected_color" : 'g',
+    "clnt_detected_val" : 1,
+    "size_multyplier" : 1000,
+    "depth_corrector_in_kb": 145
 }
 
-def isItNeededFile(entry, pattern):
+def is_it_needed_file(entry, pattern):
     return True if (entry.is_file() and fnmatch.fnmatch(entry.name, pattern)) else False
 
 
-def drawGraph(names, rng):
+def draw_graph(names, rng):
     fig, ax = plt.subplots(figsize=(19, 10))
-    ax.stackplot(names, rng, labels=[graphParams["depthCorrectorKb"]])
+    ax.stackplot(names, rng, labels=[graph_params["depth_corrector_in_kb"]])
     ax.legend(loc='upper left')
-    plt.axhline(y = graphParams["clntDetectedVal"],
-                color = graphParams["clntDetectedColor"],
+    plt.axhline(y = graph_params["clnt_detected_val"],
+                color = graph_params["clnt_detected_color"],
                 linestyle = '--')
-    # plt.axhline(y = graphParams["minTrshVal"],
-    #             color=graphParams["minTrshColor"],
+    # plt.axhline(y = graph_params["min_trsh_val"],
+    #             color=graph_params["min_trsh_color"],
     #             linestyle='--')
     plt.xticks(rotation=90)
     fig.tight_layout()
     plt.savefig(datetime.now().strftime("graphic_for_%m_%d_%Y"))
     # plt.show()
 
-def calcFileSize(size):
-    calculatedSize = (size/graphParams["sizeMultyplier"] - graphParams["depthCorrectorKb"])/10
-    return calculatedSize if calculatedSize > 0 else 0
+def calc_file_size(size):
+    calculated_size = (size/graph_params["size_multyplier"] - graph_params["depth_corrector_in_kb"])/10
+    return calculated_size if calculated_size > 0 else 0
 
 def main():
     rng = []
     names = []
     path = os.getcwd()
     pattern = "*.jpg"
-    listOfEntries = sorted(os.scandir(path), key=lambda d: d.stat().st_mtime)
+    list_of_entries = sorted(os.scandir(path), key=lambda d: d.stat().st_mtime)
 
-    for entry in listOfEntries:
-        if isItNeededFile(entry, pattern):
+    for entry in list_of_entries:
+        if is_it_needed_file(entry, pattern):
             names.append(entry.name[15:20])
             rng = np.append(rng, os.stat(entry.name).st_size)
 
     # print(rng)
     # print(names)
-    avrg = np.mean(rng)/graphParams["sizeMultyplier"]
-    graphParams["depthCorrectorKb"] = round((avrg - avrg/30), 0)
-    rng = list(map(lambda sz: calcFileSize(sz), rng))
-    drawGraph(names, rng)
+    avrg = np.mean(rng)/graph_params["size_multyplier"]
+    graph_params["depth_corrector_in_kb"] = round((avrg - avrg/30), 0)
+    rng = list(map(lambda sz: calc_file_size(sz), rng))
+    draw_graph(names, rng)
 
 main()
